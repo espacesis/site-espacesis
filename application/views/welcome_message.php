1b8584450204633060cf2ed974700d30733550c1
elsa.m <manangelsa07@gmail.com>
2018-10-25 16:27:54 +0200

    new

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,65 +1,107 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
 <head>
-<title>ESPACE ESIS</title>
-<!-- for-mobile-apps -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Travel Reservation Widget  Responsive Widget Template | Home :: w3layouts</title>
+<link href="<?=base_url("assets/css/style.css")?>" rel="stylesheet" type="text/css" media="all"/>
+<!-- Custom Theme files -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
-		function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- //for-mobile-apps -->
-<link href="assets/css/style.css" rel="stylesheet" type="text/css" media="all" />
-<link href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<link href='//fonts.googleapis.com/css?family=Quicksand:400,300,700' rel='stylesheet' type='text/css'>
+<!--google fonts-->
+<link rel="stylesheet" href="<?=base_url("assets/css/font-awesome.min.css")?>">
+<link rel="stylesheet" href="<?=base_url("assets/css/j-forms.css")?>">
+
+<script src="<?=base_url("assets/js/jquery.1.11.1.min.js")?>"></script>
+	<script src="<?=base_url("assets/js/jquery.ui.min.js")?>"></script>
+	<script src="<?=base_url("assets/js/jquery.ui.timepicker.min.js")?>"></script>
+	<script src="<?=base_url("assets/js/jquery.ui.touch-punch.min.js")?>"></script>
+	<script src="<?=base_url("assets/js/jquery.maskedinput.min.js")?>"></script>
+	<script src="<?=base_url("assets/js/jquery.validate.min.js")?>"></script>
+	<script src="<?=base_url("assets/js/jquery.form.min.js")?>"></script>
+	<script src="<?=base_url("assets/js/j-forms.min.js")?>"></script>
 </head>
 <body>
-	<div class="main">
-		<h1>Credit Card Donation Form</h1>
-		<form action="#" method="post"> 
-		<div class="w3_agile_main_grids">
-		
-			<div class="agileits_w3layouts_main_grid">
-				<div class="agile_main_grid_left">
-					
-						<h3>Your Details :</h3>
-						<input type="text" name="First Name" placeholder="First Name" required="">
-						<input type="text" name="Last Name" placeholder="Last Name" required="">
-						<input type="email" name="Email" placeholder="Email" required="">
-						<input type="text" name="Last Name" placeholder="Organization" required="">
-						<input type="text" name="Phone Number" placeholder="Address" required="">
-						
-						<select id="w3_agileits_select1" class="w3layouts_select" onchange="change_country(this.value)" required="">
-							<option value="">Country</option>
-							<option value="">America</option>
-							<option value="">Bhutan</option> 
-							<option value="">China</option>		
-							<option value="">Europe</option>
-							<option value="">Other</option>							
-						</select>
-						<select id="w3_agileits_select1" class="w3layouts_select" onchange="change_country(this.value)" required="">
-							<option value="">City</option>
-							<option value="">NewYork</option>
-							<option value="">Boston</option> 
-							<option value="">Washington</option>		
-							<option value="">Chicago</option>
-							<option value="">Other</option>							
-						</select>
-						<input type="text" name="Phone Number" placeholder="Phone Number" required="">
-						<textarea name="Message" placeholder="Comments..." required=""></textarea>
-					
+<!--header strat here-->
+<div class="element">
+	 <h1>Travel Reservation Widget</h1>
+	 
+	<div class="wrapper">
+
+		<form action="j-folder" method="post" class="j-forms j-multistep" id="j-forms" novalidate>
+
+			
+			<!-- end /.header-->
+
+			<div class="content">
+
+				<fieldset>
+					<h2>Personal Info</h2>
+					<div class="unit">
+						<div class="input">
+							<label class="icon-right" for="name">
+								<i class="fa fa-user"></i>
+							</label>
+							<input type="text" id="name" name="non" placeholder="entrez le nom">
+						</div>
 					</div>
-					
-					<div class="clear"> </div>
-					
-			</div>
-		</div>
+					<div class="unit">
+						<div class="input">
+							<label class="icon-right" for="name">
+								<i class="fa fa-user"></i>
+							</label>
+							<input type="text" id="name" name="post_nom" placeholder="entrez le post-nom">
+						</div>
+					</div>
+					<div class="unit">
+						<div class="input">
+							<label class="icon-right" for="name">
+								<i class="fa fa-user"></i>
+							</label>
+							<input type="text" id="name" name="prenon" placeholder="entrez le prenom">
+						</div>
+					</div>
+					<div class="j-row">
+						<div class="span6 unit">
+							<div class="input">
+								<label class="icon-right" for="email">
+									<i class="fa fa-envelope-o"></i>
+								</label>
+								<input type="email" id="email" name="email" placeholder="entrez email">
+							</div>
+						</div>
+						<div class="span6 unit">
+							<div class="input">
+								<label class="icon-right" for="phone">
+									<i class="fa fa-phone"></i>
+								</label>
+								<input type="text" id="phone" name="phone" placeholder="phone/mobile">
+							</div>
+						</div>
+						<div class="span6 unit">
+							<input type="submit" class="btn" value="Valider">
+						</div>
+					</div>
+				</fieldset>
+
 		</form>
-		<div class="agileits_copyright">
-			<p>© Copyright <?=date('Y')?> | Espace Esis</p>
-		</div>
 	</div>
-	<!-- Validator js -->  
-	<script src="assets/js/creditCardValidator.js" type="text/javascript"></script>
-	<!-- //Validator -->  
+<style>
+	.btn{
+		border : none;
+		background-color : rgb(0, 162, 232);
+		color: #fff;
+		padding: 1% 2%;
+		width: 10%;
+	}
+</style>
+
+	
+</div>
+<!--header end here-->
+<div class="copy-right">
+			<p>© Copyright <?=date('Y')?> | Espace Esis</p>
+</div>
 
 </body>
 </html>
