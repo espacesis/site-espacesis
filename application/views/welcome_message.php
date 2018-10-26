0742861d430d7c2f7db1a10d7e0e7ae9fe64220b
elsa.m <manangelsa07@gmail.com>
2018-10-26 15:51:13 +0200

    new

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,17 +8,17 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href='//fonts.googleapis.com/css?family=Quicksand:400,300,700' rel='stylesheet' type='text/css'>
 <!--google fonts-->
-<link rel="stylesheet" href="<?=base_url("assets/css/font-awesome.min.css")?>">
-<link rel="stylesheet" href="<?=base_url("assets/css/j-forms.css")?>">
+<link rel="stylesheet" href="<?php echo base_url("assets/css/font-awesome.min.css");?>">
+<link rel="stylesheet" href="<?php echo base_url("assets/css/j-forms.css");?>">
 
-<script src="<?=base_url("assets/js/jquery.1.11.1.min.js")?>"></script>
-	<script src="<?=base_url("assets/js/jquery.ui.min.js")?>"></script>
-	<script src="<?=base_url("assets/js/jquery.ui.timepicker.min.js")?>"></script>
-	<script src="<?=base_url("assets/js/jquery.ui.touch-punch.min.js")?>"></script>
-	<script src="<?=base_url("assets/js/jquery.maskedinput.min.js")?>"></script>
-	<script src="<?=base_url("assets/js/jquery.validate.min.js")?>"></script>
-	<script src="<?=base_url("assets/js/jquery.form.min.js")?>"></script>
-	<script src="<?=base_url("assets/js/j-forms.min.js")?>"></script>
+<script src="<?php echo base_url("assets/js/jquery.1.11.1.min.js");?>"></script>
+	<script src="<?php echo base_url("assets/js/jquery.ui.min.js");?>"></script>
+	<script src="<?php echo base_url("assets/js/jquery.ui.timepicker.min.js");?>"></script>
+	<script src="<?php echo base_url("assets/js/jquery.ui.touch-punch.min.js");?>"></script>
+	<script src="<?php echo base_url("assets/js/jquery.maskedinput.min.js");?>"></script>
+	<script src="<?php echo base_url("assets/js/jquery.validate.min.js");?>"></script>
+	<script src="<?php echo base_url("assets/js/jquery.form.min.js");?>"></script>
+	<script src="<?php echo base_url("assets/js/j-forms.min.js");?>"></script>
 </head>
 <body>
 <!--header strat here-->
@@ -27,7 +27,7 @@
 	 
 	<div class="wrapper">
 
-		<?php echo form_open('welcome/add')?>
+		<?php echo form_open('register')?>
 			
 			<!-- end /.header-->
 
@@ -40,7 +40,7 @@
 							<label class="icon-right" for="name">
 								<i class="fa fa-user"></i>
 							</label>
-							<input type="text" id="name" name="nom" placeholder="Entrez le nom" require="">
+							<input type="text" id="name" name="nom" placeholder="Entrez le nom" required="">
 						</div>
 					</div>
 					<div class="unit">
@@ -48,7 +48,7 @@
 							<label class="icon-right" for="name">
 								<i class="fa fa-user"></i>
 							</label>
-							<input type="text" id="name" name="prenom" placeholder="Entrez le prenom" require="">
+							<input type="text" id="name" name="prenom" placeholder="Entrez le prenom" required="">
 						</div>
 					</div>
 					<div class="unit">
@@ -56,16 +56,20 @@
 							<label class="icon-right" for="name">
 								<i class="fa fa-user"></i>
 							</label>
-							<input type="text" id="name" name="promotion" placeholder="Entrez la promotion" require="">
+							<select id="name" name="promotion" placeholder="Entrez la promotion" required="">
+								<option>PREPA</option>
+								<option>G1</option>
+								<option>G2 SI</option>
+								<option>G2 GST</option>
+							</select>
 						</div>
 					</div>
-					<div class="j-row">
 						<div class="span6 unit">
 							<div class="input">
 								<label class="icon-right" for="email">
 									<i class="fa fa-envelope-o"></i>
 								</label>
-								<input type="text" id="name" name="matricule" placeholder="Entrer le matricule" require="">
+								<input type="text" id="name" name="matricule" placeholder="Entrer le matricule" required="">
 							</div>
 						</div>
 						<div class="span6 unit">
@@ -73,13 +77,12 @@
 								<label class="icon-right" for="phone">
 									<i class="fa fa-phone"></i>
 								</label>
-								<input type="text" id="phone" name="phone" placeholder="phone/mobile" require="">
+								<input type="text" id="phone" name="phone" placeholder="phone/mobile" required="">
 							</div>
 						</div>
 						<div class="span6 unit">
 							<input type="submit" class="btn" value="valider">
 						</div>
-					</div>
 				</fieldset>
 			</div>
 		</form>
