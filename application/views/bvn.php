ab965b083e4157f5107c15cae7f77fbd3248bd2a
elsa.m <manangelsa07@gmail.com>
2018-11-02 08:54:24 +0100

    new

---
+++ application/views/bvn.php
@@ -0,0 +1,5 @@
+<?php session_start();
+?>
+<section class="ftco-section">
+    <p>Bonjour <?php echo ." ".$name_new_membre." ".$last_name_new_membre." "; ?> bienvenue dans le club des passionées de la programmation, nous sommes content de t'acceuillir</p>
+</section>
