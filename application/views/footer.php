40cf74c4c3973637a1c1f8258d986227754cafd7
elsa.m <manangelsa07@gmail.com>
2018-11-02 08:54:35 +0100

    [update]

---
+++ application/views/footer.php
@@ -0,0 +1,39 @@
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | ESPACE ESIS</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="<?php echo base_url("assets/js/jquery.min.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/jquery-migrate-3.0.1.min.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/popper.min.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/jquery.easing.1.3.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/jquery.waypoints.min.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/jquery.stellar.min.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/owl.carousel.min.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/jquery.magnific-popup.min.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/aos.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/jquery.animateNumber.min.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/bootstrap-datepicker.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/jquery.timepicker.min.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/jquery.mb.YTPlayer.min.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/scrollax.min.js"); ?>"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="<?php echo base_url("assets/js/google-map.js"); ?>"></script>
+  <script src="<?php echo base_url("assets/js/main.js"); ?>"></script>
+    
+  </body>
+</html>
