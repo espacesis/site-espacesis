587280c251789f31282e07cf9dc6fa6e0947a315
elsa.m <manangelsa07@gmail.com>
2018-11-02 08:54:55 +0100

    [update]

---
+++ application/views/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>ESPACE ESIS</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/open-iconic-bootstrap.min.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/animate.css"); ?>">
+    
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/owl.carousel.min.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/owl.theme.default.min.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/magnific-popup.css"); ?>">
+
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/aos.css"); ?>">
+
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/ionicons.min.css"); ?>">
+
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap-datepicker.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/jquery.timepicker.css"); ?>">
+
+    
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/flaticon.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/icomoon.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
+  </head>
+  <body>
+    
+	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.html"><img src="<?php echo base_url("assets/images/logo.png") ;?>" width="10%"><span> Espace<br>Esis</span> </a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="blog.html" class="nav-link">Forum</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+
+    <section id="home" class="video-hero js-fullheight" style="height: 100%; background-image: url(<?php echo base_url("assets/images/bg_1.jpg"); ?>);  background-size:cover; background-position: center center;background-attachment:fixed;" data-section="home">
+			<div class="overlay js-fullheight"></div>
+			<a class="player" data-property="{videoURL:'https://www.youtube.com/watch?v=5m--ptwd_iI',containment:'#home', showControls:false, autoPlay:true, loop:true, mute:false, startAt:0, opacity:1, quality:'default'}"></a>
+			<div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
+          <div class="col-md-10 ftco-animate text-center" data-scrollax=" properties: { translateY: '70%' }">
+            <h1 class="mb-4" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Club <strong>Des Passionnés</strong> Du Code</h1>
+            
+          </div>
+        </div>
+      </div>
+		</section>
